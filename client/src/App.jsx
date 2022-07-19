ca351784a68ffb34884fca778357325edfbb23d7
im-ashish00 <iamashishsharma00@gmail.com>
2022-07-19 16:00:42 +0530

    Added Style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import TextEditor from './TextEditor';
+import './App.css';
 
 export default function App() {
-  return <div>Hello 👋</div>;
+  return (
+    <>
+      <TextEditor />
+    </>
+  );
 }
--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -2,13 +2,25 @@ import React, { useCallback } from 'react';
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
 
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  ['bold', 'italic', 'underline'],
+  [{ color: [] }, { background: [] }],
+  [{ script: 'sub' }, { script: 'super' }],
+  [{ align: [] }],
+  ['image', 'blockquote', 'code-block'],
+  ['clean'],
+];
+
 export default function TextEditor() {
   const containerRef = useCallback((containerElement) => {
     if (containerElement == null) return;
     containerElement.innerHTML = '';
     const editor = document.createElement('div');
     containerElement.append(editor);
-    new Quill(editor, { theme: 'snow' });
+    new Quill(editor, { theme: 'snow', modules: { toolbar: TOOLBAR_OPTIONS } });
   }, []);
   return <div ref={containerRef}></div>;
 }
